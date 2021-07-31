516661b89bcdef201b5078ec9e6db79c96d0c9f1
bluewhale-ikj <bluewhale.ikj@gmail.com>
2021-08-01 01:10:17 +0900

    Add and fix some comments

--- issue_writer.py
+++ issue_writer.py
@@ -1,6 +1,6 @@
 # 1. issue data가 담긴 csv 파일의 이름은 issues.csv로 할 것
 # 2. issue data는 순서대로 issue number, tracker, category, subject 4개의 필드만을 갖는다.
-# 3. issues.csv는 release_writer와 같은 directory 내에 위치할 것
+# 3. issues.csv는 issue_writer.py와 같은 directory 내에 위치할 것
 # 4. 프로그램을 실행하면 'release_draft.txt'가 생성된다.
 
 import csv
@@ -10,6 +10,7 @@ write_file = open('release_draft.txt', 'w')
 file = csv.reader(read_file)
 
 for line in file:
+    # The first row of the issue.csv
     if line[0] == '\ufeff#' or line[0] == '#':
         header = line
     else:
